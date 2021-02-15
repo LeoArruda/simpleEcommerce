6599fd916f27b1d82d8cc878aee60b44b1530a41
Leo Arruda <lvarruda@gmail.com>
2021-02-14 19:40:07 -0500

    feat: add OrderRepository interface

--- src/main/java/com/brainelixir/ecomm/repositories/OrderRepository.java
+++ src/main/java/com/brainelixir/ecomm/repositories/OrderRepository.java
@@ -1,10 +1,11 @@
 package com.brainelixir.ecomm.repositories;
 
-import com.brainelixir.ecomm.models.Product;
+
+import com.brainelixir.ecomm.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository("productRepository")
-public interface ProductRepository extends JpaRepository<Product,Long> {
+@Repository("orderRepository")
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
