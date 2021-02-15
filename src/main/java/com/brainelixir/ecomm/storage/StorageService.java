e4bea3b8962671c428dcbfbb81368c37bd2f9b85
Leo Arruda <lvarruda@gmail.com>
2021-02-14 21:09:56 -0500

    feat: add Storage Service

--- src/main/java/com/brainelixir/ecomm/storage/StorageService.java
+++ src/main/java/com/brainelixir/ecomm/storage/StorageService.java
@@ -1,2 +1,22 @@
-package com.brainelixir.ecomm.storage;public interface StorageService {
+package com.brainelixir.ecomm.storage;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public interface StorageService {
+
+    void init();
+
+    String store(MultipartFile file, String path);
+
+    Stream<Path> loadAll();
+
+    Path load(String filename);
+
+    Resource loadAsResource(String filename);
+
+    void deleteAll();
 }
